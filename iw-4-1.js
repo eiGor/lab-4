ffd4bf15e00f5e82ffa252cb9039d072d09b41dc
Gor <fedvel.1900@gmail.com>
2020-04-15 13:16:22 +0300

    'IW-1_fin'

--- iw-4-1.js
+++ iw-4-1.js
@@ -1,2 +1,19 @@
-var v = 'IW git test.';
-$('#out').text(v);
+let inputString = 'Осуществить невозможно, задача слишком сложная.';
+let words = inputString.split(/[ ,.]+/);
+
+//words.forEach(w => console.log(w.slice(-2)));
+
+var ver = 0, adver= 0, adj = 0;
+
+for (w of words) {
+    let lastLet = w.slice(-1);
+
+    if (lastLet == 'о')
+        adver++;
+    else if (lastLet == 'ь')
+        ver++;
+    else if (w.slice(-2) =='ая')
+        adj++;
+}
+
+$('#out').text('Наречий ' + adver + ', глаголов ' + ver + ', прилагательных ' + adj + '.');
